cd1e7d1af5fd9f9a3a6cb2a8d4a9fbb587ce828d
Daniil Barelnikov <107426665+Barelnikovs@users.noreply.github.com>
2024-11-30 20:44:18 +0300

    Explore_USA_0.9.0

--- js/loadBanner.js
+++ js/loadBanner.js
@@ -23,90 +23,90 @@ const images = ['./image/banner/Atlanta.jpg',
                       './image/banner/Seattle.jpg',
                        './image/banner/Washington_DC.jpg', ]
 const randomImage = Math.floor(Math.random() * images.length)
-
+const linearGradient = 'linear-gradient(to bottom, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.2), transparent, transparent, transparent), '
 switch (title) {
      case 'Explore USA main': {
-          banner.style.backgroundImage = `url(${images[randomImage]})`
+          banner.style.background = `${linearGradient}url(${images[randomImage]})`
           break
      }
      case 'Атланта штат Джорджия': {
-          banner.style.backgroundImage = `url(.${images[0]})`
+          banner.style.backgroundImage = `${linearGradient}url(.${images[0]})`
           break
      }
      case 'Бостон штат Массачусетс': {
-          banner.style.backgroundImage = `url(.${images[1]})`
+          banner.style.backgroundImage = `${linearGradient}url(.${images[1]})`
           break
      }
      case 'Чикаго штат Иллинойс': {
-          banner.style.backgroundImage = `url(.${images[2]})`
+          banner.style.backgroundImage = `${linearGradient}url(.${images[2]})`
           break
      }
      case 'Даллас штат Техас': {
-          banner.style.backgroundImage = `url(.${images[3]})`
+          banner.style.backgroundImage = `${linearGradient}url(.${images[3]})`
           break
      }
      case 'Детроит штат Мичиган': {
-          banner.style.backgroundImage = `url(.${images[4]})`
+          banner.style.backgroundImage = `${linearGradient}url(.${images[4]})`
           break
      }
      case 'Хьюстон штат Техас': {
-          banner.style.backgroundImage = `url(.${images[5]})`
+          banner.style.backgroundImage = `${linearGradient}url(.${images[5]})`
           break
      }
      case 'Лос Анджелес штат Калифорния': {
-          banner.style.backgroundImage = `url(.${images[6]})`
+          banner.style.backgroundImage = `${linearGradient}url(.${images[6]})`
           break
      }
      case 'Лас вегас штат Невада': {
-          banner.style.backgroundImage = `url(.${images[7]})`
+          banner.style.backgroundImage = `${linearGradient}url(.${images[7]})`
           break
      }
      case 'Майями штат Флорида': {
-          banner.style.backgroundImage = `url(.${images[8]})`
+          banner.style.backgroundImage = `${linearGradient}url(.${images[8]})`
           break
      }
      case 'Филадельфия штат Пенсильвания': {
-          banner.style.backgroundImage = `url(.${images[12]})`
+          banner.style.backgroundImage = `${linearGradient}url(.${images[12]})`
           break
      }
      case 'Миннеаполис штат Миннесота': {
-          banner.style.backgroundImage = `url(.${images[9]})`
+          banner.style.backgroundImage = `${linearGradient}url(.${images[9]})`
           break
      }
      case 'Новый Орлеан штат Луизиана': {
-          banner.style.backgroundImage = `url(.${images[10]})`
+          banner.style.backgroundImage = `${linearGradient}url(.${images[10]})`
           break
      }
      case 'Нью-Йорк штат Нью-Йорк': {
-          banner.style.backgroundImage = `url(.${images[11]})`
+          banner.style.backgroundImage = `${linearGradient}url(.${images[11]})`
           break
      }
      case 'Феникс штат Аризона': {
-          banner.style.backgroundImage = `url(.${images[13]})`
+          banner.style.backgroundImage = `${linearGradient}url(.${images[13]})`
           break
      }
      case 'Сан Антонио штат Техас': {
-          banner.style.backgroundImage = `url(.${images[14]})`
+          banner.style.backgroundImage = `${linearGradient}url(.${images[14]})`
           break
      }
      case 'Сан Диего штат Калифорния': {
-          banner.style.backgroundImage = `url(.${images[15]})`
+          banner.style.backgroundImage = `${linearGradient}url(.${images[15]})`
           break
      }
      case 'Сан Франциско штат Калифорния': {
-          banner.style.backgroundImage = `url(.${images[16]})`
+          banner.style.backgroundImage = `${linearGradient}url(.${images[16]})`
           break
      }
      case 'Сан Хосе штат Калифорния': {
-          banner.style.backgroundImage = `url(.${images[17]})`
+          banner.style.backgroundImage = `${linearGradient}url(.${images[17]})`
           break
      }
      case 'Сиэтл штат Вашингтон': {
-          banner.style.backgroundImage = `url(.${images[18]})`
+          banner.style.backgroundImage = `${linearGradient}url(.${images[18]})`
           break
      }
      case 'Вашингтон округ Колумбия': {
-          banner.style.backgroundImage = `url(.${images[19]})`
+          banner.style.backgroundImage = `${linearGradient}url(.${images[19]})`
           break
      }
 }
